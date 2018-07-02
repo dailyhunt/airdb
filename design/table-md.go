b8f3a6df5566058357f99fdcb6c1c0e4ed7907d3
sohanvir <sohimankotia@gmail.com>
2018-07-02 14:34:47 +0530

    Adding table api draft

---
+++ design/table-md.go
@@ -0,0 +1,49 @@
+package design
+
+type Slice []byte
+
+type Cell struct {
+	key    Slice
+	family Slice
+	col    Slice
+}
+
+type Put struct {
+	Cell
+	value Slice
+}
+
+type Get struct {
+	Cell
+	value Slice
+}
+
+type Incr struct {
+	Cell
+	incrBy Slice
+}
+
+type Decr struct {
+	Cell
+	decrBy Slice
+}
+
+type Decay struct {
+	Cell
+	formula func(prevVal Slice)
+}
+
+type tableop interface {
+	// Data Operation
+	Get(gets ...Get) []Get
+	Put(puts ...Put) error //Todo :  should create column on the fly ??
+	Incr(incrs ...Incr)
+	Decr(decrs ...Decr)
+	Decay(decays ...Decay)
+
+	// Admin Operations
+	Open()
+	Close()
+	Disable() // Temporary Disable Table for any operation
+	Archive() // Delete Table
+}
